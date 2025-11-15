19813bd94a8e4a426a8ace9502f7a6b9fe63d883
Bunthoeun Kong <amcjkbtk91@outlook.com>
2025-11-15 19:37:42 +0100

    Add Dashboard page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,6 +2,8 @@
 
 import { useState } from "react";
 import PageWrapper from "@/components/PageWrapper";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function CreateRdvPage() {
   const [message, setMessage] = useState("");
@@ -24,19 +26,40 @@ export default function CreateRdvPage() {
   return (
     <PageWrapper>
       <div className="md:w-lg mx-auto w-full p-4 text-black">
-        {message && (
-          <div className="bg-green-100 text-green-700 p-3 rounded mb-4">
-            {message}
-          </div>
-        )}
-        {error && (
-          <div className="bg-red-100 text-red-700 p-3 rounded mb-4">
-            {error}
-          </div>
-        )}
+        <br />
+        <br />
+        <br />
 
-        <form action={handleSubmit} className="space-y-4 p-6 rounded-md">
-          <h2 className="text-2xl font-bold mb-6">S'identifier</h2>
+        <div className="flex justify-center">
+          <Image
+            src="/logo.png"
+            width={200}
+            height={200}
+            alt="logo"
+            loading="eager"
+          />
+        </div>
+
+        <br />
+
+        <h2 className="text-2xl font-bold mb-6 uppercase text-center">
+          S'identifier
+        </h2>
+
+        <form
+          action={handleSubmit}
+          className="space-y-4 p-6 bg-white rounded-2xl"
+        >
+          {message && (
+            <div className="bg-green-100 text-green-700 p-3 rounded mb-4">
+              {message}
+            </div>
+          )}
+          {error && (
+            <div className="bg-red-100 text-red-700 p-3 rounded mb-4">
+              {error}
+            </div>
+          )}
 
           <div>
             <input
@@ -45,7 +68,7 @@ export default function CreateRdvPage() {
               name="email"
               required
               placeholder="Email"
-              className="w-full p-3 rounded shadow bg-white"
+              className="w-full p-3 shadow bg-[#D9D9D9] rounded-xl outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#3C6E71]"
             />
           </div>
 
@@ -56,17 +79,23 @@ export default function CreateRdvPage() {
               name="password"
               required
               placeholder="Mot de passe"
-              className="w-full p-3 rounded shadow bg-white"
+              className="w-full p-3 shadow bg-[#D9D9D9] rounded-xl outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#3C6E71]"
             />
           </div>
 
           <button
             type="submit"
-            className="w-full p-3 rounded shadow font-bold uppercase cursor-pointer text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            className="w-full mt-3 p-3.5 rounded-xl shadow font-bold uppercase cursor-pointer text-white bg-[#284B63] hover:bg-[#3C6E71] focus:ring-2 focus:ring-offset-2 focus:ring-[#3C6E71]"
           >
             S'identifier
           </button>
         </form>
+
+        <br />
+
+        <Link href="#" className="block text-center">
+          Mot de passe oublié ?
+        </Link>
       </div>
     </PageWrapper>
   );
